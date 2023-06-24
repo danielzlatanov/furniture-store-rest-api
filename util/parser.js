05db5eaea2223115b8a8762876585225da66f199
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-06-24 19:57:45 +0300

    fix error parser issue

--- util/parser.js
+++ util/parser.js
@@ -1,9 +1,9 @@
 function parseError(error) {
 	if (Array.isArray(error)) {
 		return error.map(e => e.msg).join('\n');
-	} else if ((error.name = 'ValidationError')) {
+	} else if (error.name == 'ValidationError') {
 		return Object.values(error.errors)
-			.map(v => v.msg)
+			.map(v => v.message)
 			.join('\n');
 	} else {
 		return error.message;
