261599a52ccc2ac9aaa2f71ea64a60cbacb65726
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-06-24 18:40:46 +0300

    add error parser and auth controller validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,36 @@
+const { body, validationResult } = require('express-validator');
 const { register, login, logout } = require('../services/userService.js');
+const { parseError } = require('../util/parser.js');
 
 const authController = require('express').Router();
 
-authController.post('/register', async (req, res) => {
-	try {
-		const token = await register(req.body.email, req.body.password);
-		res.json(token);
-	} catch (error) {
-		res.status(400).json({ message: error.message });
+authController.post(
+	'/register',
+	body('email').isEmail().withMessage('Please enter a valid email address'),
+	body('password').isLength({ min: 3 }).withMessage('Password must be at least 3 characters long'),
+	async (req, res) => {
+		try {
+			const { errors } = validationResult(req);
+			if (errors.length > 0) {
+				throw errors;
+			}
+
+			const token = await register(req.body.email, req.body.password);
+			res.json(token);
+		} catch (error) {
+			const message = parseError(error);
+			res.status(400).json({ message });
+		}
 	}
-});
+);
 
 authController.post('/login', async (req, res) => {
 	try {
 		const token = await login(req.body.email, req.body.password);
 		res.json(token);
 	} catch (error) {
-		res.status(401).json({ message: error.message });
+		const message = parseError(error);
+		res.status(401).json({ message });
 	}
 });
 
