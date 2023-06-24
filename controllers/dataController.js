97eb665511d4f6447141a0dc2f271975437a326a
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-06-24 20:01:36 +0300

    implemented create furniture functionality

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,12 +1,22 @@
+const { getAll, create } = require('../services/itemService.js');
+const { parseError } = require('../util/parser.js');
+
 const dataController = require('express').Router();
 
-dataController.get('/', (req, res) => {
-	res.json([]);
+dataController.get('/', async (req, res) => {
+	const items = await getAll();
+	res.json(items);
 });
 
-dataController.post('/', (req, res) => {
-	console.log(req.body);
-	res.end();
+dataController.post('/', async (req, res) => {
+	try {
+		const data = Object.assign({ _ownerId: req.user._id }, req.body);
+		const item = await create(data);
+		res.json(item);
+	} catch (error) {
+		const message = parseError(error);
+		res.status(400).json({ message });
+	}
 });
 
 module.exports = dataController;
