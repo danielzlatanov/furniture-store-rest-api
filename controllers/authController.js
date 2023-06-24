0540502d4e6b362c9619295d81332f6ba2c3ef65
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-06-24 18:15:09 +0300

    implemented user login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,22 @@
-const { register } = require('../services/userService.js');
+const { register, login } = require('../services/userService.js');
 
 const authController = require('express').Router();
 
 authController.post('/register', async (req, res) => {
 	try {
 		const token = await register(req.body.email, req.body.password);
-    res.json(token);
+		res.json(token);
 	} catch (error) {
 		res.status(400).json({ message: error.message });
+	}
+});
+
+authController.post('/login', async (req, res) => {
+	try {
+		const token = await login(req.body.email, req.body.password);
+		res.json(token);
+	} catch (error) {
+		res.status(401).json({ message: error.message });
 	}
 });
 
--- services/userService.js
+++ services/userService.js
@@ -15,14 +15,22 @@ async function register(email, password) {
 		hashedPassword: await bcrypt.hash(password, 10),
 	});
 
-	return {
-		_id: user._id,
-		email: user.email,
-		accessToken: createToken(user),
-	};
+	return createToken(user);
 }
 
-async function login(email, password) {}
+async function login(email, password) {
+	const user = await User.findOne({ email }).collation({ locale: 'en', strength: 2 });
+	if (!user) {
+		throw new Error('Incorrect email or password');
+	}
+
+	const match = await bcrypt.compare(password, user.hashedPassword);
+	if (!match) {
+		throw new Error('Incorrect email or password');
+	}
+
+	return createToken(user);
+}
 
 async function logout() {}
 
@@ -32,7 +40,11 @@ function createToken({ _id, email }) {
 		email,
 	};
 
-	return jwt.sign(payload, jwtSecret);
+	return {
+		_id,
+		email,
+		accessToken: jwt.sign(payload, jwtSecret),
+	};
 }
 
 function parseToken(token) {}
